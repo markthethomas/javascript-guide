e2590e12d286a64fad771f93cd9d6498b38383df
Mark Thomas <markthethomas@gmail.com>
2016-01-05 20:54:27 -0800

    Add more cases for how  gets defined

--- this/this.js
+++ this/this.js
@@ -46,6 +46,90 @@ test('this when called from arrow function as property of an object', t => {
 test('this when called from arrow function via call/apply/bind', t => {
   const anObject = {};
   const arrow = (() => this);
-  t.equals(arrow.call(anObject), this);
+  t.equals(arrow.call(anObject), this, 'The lexical this cannot be overriden with call');
+  t.equals(arrow.apply(anObject), this, 'The lexical this cannot be overriden with apply');
+  t.equals(arrow.bind(anObject)(), this, 'The lexical this cannot be overriden with bind');
   t.end();
 });
+
+test('this inside of an object', t => {
+  const anotherObject = {
+    returnInnerThis: function() {
+      return this;
+    },
+    returnFromArrow: () => this
+  }
+  t.equals(anotherObject.returnInnerThis(), anotherObject, 'Non-arrow functions should have a this of the surrounding object');
+  t.equals(anotherObject.returnFromArrow(), this, 'Should have the same this as the surrounding context');
+  t.end();
+});
+
+
+test('this as an object method', t => {
+  const monkey = {
+    tails: 1,
+    howManyTails: function() {
+      return this.tails
+    },
+  }
+  t.equals(monkey.howManyTails(), 1, 'this is set to the calling object; you can access other props on the object on it')
+
+  const tiger = {
+    tails: 1,
+  }
+
+  function countTails() {
+    return this.tails;
+  }
+
+  tiger.count = countTails;
+
+  t.equals(tiger.count(), 1, 'Execution site is what matters, not definition site')
+  t.end();
+});
+
+test('this should only follow the most immediate reference', t => {
+  function getTheTruth() {
+    return this.truth;
+  }
+  const deepObject = {
+    a: {
+      b: {
+        truth: false,
+        c: {
+          truth: true,
+          d: {
+            truth: null
+          }
+        }
+      }
+    }
+  }
+
+  // Assign our method to the various levels of the object
+  deepObject.a.b.getTheTruth = getTheTruth;
+  deepObject.a.b.c.getTheTruth = getTheTruth;
+  deepObject.a.b.c.d.getTheTruth = getTheTruth;
+
+  // Only the latest member matters for this
+  t.equal(deepObject.a.b.getTheTruth(), false, 'Should be false');
+  t.equal(deepObject.a.b.c.getTheTruth(), true, 'Should be true');
+  t.equal(deepObject.a.b.c.d.getTheTruth(), null, 'Should be null');
+  t.end();
+});
+
+test("this on the object's prototype chain", t => {
+  const animal = {
+    sound: 'mooooooooooooo',
+    makeSound: function() {
+      return this.sound;
+    }
+  }
+
+  let Cow = Object.create(animal);
+  Cow.spots = 3;
+  Cow.hooves = 3; // sorry, Cow
+  t.equals(Cow.makeSound(), 'mooooooooooooo');
+  t.end();
+});
+
